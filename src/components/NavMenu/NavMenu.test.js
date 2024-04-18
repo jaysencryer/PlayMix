c144cfda79ba11ba5bfcc9de80c81a8bf3cc142d
Jaysen Cryer <jaysen.cryer@daugherty.com>
2024-04-17 21:46:49 -0500

    updated NavMenu.test.js

--- src/components/NavMenu/NavMenu.test.js
+++ src/components/NavMenu/NavMenu.test.js
@@ -2,20 +2,33 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen, fireEvent } from '@testing-library/react';
 
+import { SpotifyProvider } from '../../context/SpotifyContext';
 import NavMenu from './NavMenu';
+
+const mockProfile = {
+  spotifyProfile: { user: 'test', avatar: 'test.jpg' },
+};
 
 describe('NavMenu tests', () => {
   test('NavMenu renders', () => {
-    render(<NavMenu user="test" avatar="mockAvatar" setView={jest.fn()} />);
+    render(
+      <SpotifyProvider profile={mockProfile}>
+        <NavMenu setView={jest.fn()} />
+      </SpotifyProvider>,
+    );
   });
 
   test('NavMenu when I click a button setView is called', () => {
     const mockMenu = jest.fn();
-    render(<NavMenu user="test" avatar="mockAvatar" setView={mockMenu} />);
+    render(
+      <SpotifyProvider profile={mockProfile}>
+        <NavMenu setView={mockMenu} />
+      </SpotifyProvider>,
+    );
     const navButtons = screen.getAllByRole('button');
     navButtons.forEach((button) => {
       fireEvent.click(button);
     });
-    expect(mockMenu).toBeCalledTimes(4);
+    expect(mockMenu).toBeCalledTimes(3);
   });
 });
